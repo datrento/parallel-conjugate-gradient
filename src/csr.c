79db164941b7d2295a3f87eacbf5f3e907bb9a36
abiget <getachewante@gmail.com>
2025-12-26 16:05:12 +0100

    feature: csr based jacobi conjugate gradient parallel done.

--- src/csr.c
+++ src/csr.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
+#include <omp.h>
 
-void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local)
+void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local, int rank)
 {
+    /**
+     * Extracts the diagonal elements from the local CSR matrix A_local.
+     * A_local: local CSR matrix
+     * diag_A_local: output array to store diagonal elements (size n_local)
+     */
     for (int r = 0; r < A_local->n_local; r++)
     {
         int global_row = A_local->row_start + r;
@@ -23,7 +29,8 @@ void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local)
 
         if (diag_value == 0.0)
         {
-            fprintf(stderr, "Warning: Zero diagonal entry found at global row %d\n", global_row);
+            fprintf(stderr, "[rank %d]Warning: Zero diagonal entry found at global row %d\n", rank, global_row);
+            fflush(stderr);
         }
 
         diag_A_local[r] = diag_value;
@@ -32,6 +39,13 @@ void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local)
 
 void csr_sparse_matvec_mult_local(const CSR *A_local, const double *x_full, double *y_local)
 {
+    /**
+     * Performs sparse matrix-vector multiplication y_local = A_local * x_full
+     * A_local: local CSR matrix
+     * x_full: full input vector (size n)
+     * y_local: output vector (size n_local)
+     */
+    // #pragma omp parallel for schedule(static)
     for (int i = 0; i < A_local->n_local; i++)
     {
         double sum = 0.0;
@@ -43,31 +57,52 @@ void csr_sparse_matvec_mult_local(const CSR *A_local, const double *x_full, doub
     }
 }
 
-int csr_distribute(CSR *A_full, CSR *A_local, int rank, int size, MPI_Comm comm)
+void print_csr_memory_usage(int rank, size_t nnz, int n, const char *label)
 {
-    int n;
+    /***
+     * Prints the memory usage of a CSR matrix given its number of non-zero entries (nnz).
+     * rank - MPI rank for logging purposes
+     ***/
+    size_t int_size = sizeof(int);
+    size_t double_size = sizeof(double);
 
-    // Broadcast global matrix size
-    if (rank == 0)
-    {
-        n = A_full->n;
-    }
+    size_t row_ptr_size = (nnz + 1) * int_size;
+    size_t col_indices_size = nnz * int_size;
+    size_t values_size = nnz * double_size;
 
-    MPI_Bcast(&n, 1, MPI_INT, 0, comm);
+    double mb = 1024.0 * 1024.0;
+    size_t total_size = row_ptr_size + col_indices_size + values_size;
+
+    // dense total size
+    size_t dense_size = n * n * double_size;
+    // compare it with the denser matrix memory usage use TB for the full dense matrix
+    printf("[rank %d] %s: values_mem=%.2f MB col_indices_mem=%.2f MB row_ptr_mem=%.2f MB total_mem=%.2f GB dense_equiv_mem=%.2f TB\n",
+           rank, label, (double)values_size / mb, (double)col_indices_size / mb,
+           (double)row_ptr_size / mb, (double)total_size / (mb * 1024),
+           (double)dense_size / (mb * 1024 * 1024));
+    fflush(stdout);
+}
+
+int csr_distribute(
+    CSR *A_full, CSR *A_local, int n,
+    int n_local, int row_start,
+    int rank, int size, MPI_Comm comm)
+{
+
     A_local->n = n;
 
     // Compute row distribution for All ranks
     int base = n / size;
     int remainder = n % size;
-    A_local->n_local = (rank < remainder) ? base + 1 : base;
-    A_local->row_start = rank * base + (rank < remainder ? rank : remainder); // starting row index in global matrix
+    A_local->n_local = n_local;
+    A_local->row_start = row_start; // starting row index in global matrix
 
     // Step 1: Scatter row_ptr segments
     int *local_row_ptr_temp = (int *)malloc((A_local->n_local + 1) * sizeof(int));
 
     if (!local_row_ptr_temp)
     {
-        fprintf(stderr, "Error allocating memory for local_row_ptr_temp\n");
+        fprintf(stderr, "[rank %d] Error allocating memory for local_row_ptr_temp\n", rank);
         return -1;
     }
 
@@ -84,93 +119,93 @@ int csr_distribute(CSR *A_full, CSR *A_local, int rank, int size, MPI_Comm comm)
             displs[i] = start;
         }
 
+        // Distribute A_full->row_ptr to all ranks' local_row_ptr_temp
         MPI_Scatterv(A_full->row_ptr, sendcounts, displs, MPI_INT,
                      local_row_ptr_temp, A_local->n_local + 1, MPI_INT,
                      0, comm);
 
+        // Free allocation arrays
         free(sendcounts);
         free(displs);
     }
     else
     {
+        // Receive row_ptr segments
         MPI_Scatterv(NULL, NULL, NULL, MPI_INT,
                      local_row_ptr_temp, A_local->n_local + 1, MPI_INT,
                      0, comm);
     }
 
-    // Adjust local_row_ptr to start from 0
+    // Adjust local_row_ptr to start from 0 for each rank and compute local nnz(non zeros)
     int nnz_offset = local_row_ptr_temp[0];
     A_local->nnz = local_row_ptr_temp[A_local->n_local] - nnz_offset;
 
-    // allocate row_ptr for A_local
+    // Allocate A_local->row_ptr to store adjusted row_ptr
     A_local->row_ptr = (int *)malloc((A_local->n_local + 1) * sizeof(int));
 
+    // Check allocation success
     if (!A_local->row_ptr)
     {
-        fprintf(stderr, "Error allocating memory for local row_ptr\n");
+        fprintf(stderr, "[rank %d] Error allocating memory for local row_ptr\n", rank);
         free(local_row_ptr_temp);
         return -1;
     }
 
-    // Adjust row_ptr to start from 0
+    // Adjust A_local->row_ptr to start from 0 for each rank
     for (int i = 0; i < A_local->n_local + 1; i++)
     {
         A_local->row_ptr[i] = local_row_ptr_temp[i] - nnz_offset;
     }
 
+    // Free temporary local_row_ptr storage
     free(local_row_ptr_temp);
-    // Debug: print allocation size
-    if (rank == 0)
-    {
-        printf("Rank 0: nnz=%d, allocating col_indices (%d ints), values (%d doubles)\n",
-               A_local->nnz, A_local->nnz, A_local->nnz);
-        double total_size = A_local->nnz * (sizeof(int) + sizeof(double));
-        printf("Rank 0: Total allocation size for col_indices and values: %.2f MB\n",
-               total_size / (1024.0 * 1024.0));
-    }
 
     // Step 2: Scatter col_indices and values based on each rank's nnz
     // allocate col_indices and values for A_local
     A_local->col_indices = (int *)malloc(A_local->nnz * sizeof(int));
+    A_local->values = (double *)malloc(A_local->nnz * sizeof(double));
 
     if (!A_local->col_indices)
     {
-        fprintf(stderr, "Rank %d: malloc failed for col_indices (need %d ints = %.2f MB)\n",
+        fprintf(stderr, "[rank %d] malloc failed for col_indices (need %d ints = %.2f MB)\n",
                 rank, A_local->nnz, A_local->nnz * sizeof(int) / (1024.0 * 1024.0));
         free(A_local->row_ptr);
         return -1;
     }
 
-    A_local->values = (double *)malloc(A_local->nnz * sizeof(double));
-
     if (!A_local->values)
     {
-        fprintf(stderr, "Rank %d: malloc failed for values (need %d doubles = %.2f MB)\n",
+        fprintf(stderr, "[rank %d] malloc failed for values (need %d doubles = %.2f MB)\n",
                 rank, A_local->nnz, A_local->nnz * sizeof(double) / (1024.0 * 1024.0));
         free(A_local->row_ptr);
         free(A_local->col_indices);
         return -1;
     }
 
+    // Print memory usage for local CSR matrix per rank
+    print_csr_memory_usage(rank, A_local->nnz, A_local->n_local, "local");
+
     if (rank == 0)
     {
         // Prepare sendcounts and displs for col_indices and values
         int *sendcounts_nnz = (int *)malloc(size * sizeof(int));
         int *displs_nnz = (int *)malloc(size * sizeof(int));
+
         for (int i = 0; i < size; i++)
         {
             // let say remainder = 2, base = 3, size = 5
-            // p0_start = 0 * 3 + 0 = 0,    count = 4
-            // p1_start = 1 * 3 + 1 = 4,    count = 4
-            // p2_start = 2 * 3 + 2 = 8,    count = 3
-            // p3_start = 3 * 3 + 2 = 11,   count = 3
-            // p4_start = 4 * 3 + 2 = 14,   count = 3
+            // p0_start = 0 * 3 + 0 = 0,    count = 4 => sendcounts_nnz[0] = 4
+            // p1_start = 1 * 3 + 1 = 4,    count = 4 => sendcounts_nnz[1] = 4
+            // p2_start = 2 * 3 + 2 = 8,    count = 3 => sendcounts_nnz[2] = 3
+            // p3_start = 3 * 3 + 2 = 11,   count = 3 => sendcounts_nnz[3] = 3
+            // p4_start = 4 * 3 + 2 = 14,   count = 3 => sendcounts_nnz[4] = 3
             int start = i * base + (i < remainder ? i : remainder);
             int count = ((i < remainder) ? (base + 1) : base);
             sendcounts_nnz[i] = A_full->row_ptr[start + count] - A_full->row_ptr[start]; // start to end nnz for this rank
             displs_nnz[i] = A_full->row_ptr[start];
         }
 
+        // Distribute col_indices and values to all ranks' A_local
         MPI_Scatterv(A_full->col_indices, sendcounts_nnz, displs_nnz, MPI_INT,
                      A_local->col_indices, A_local->nnz, MPI_INT,
                      0, comm);
@@ -179,11 +214,13 @@ int csr_distribute(CSR *A_full, CSR *A_local, int rank, int size, MPI_Comm comm)
                      A_local->values, A_local->nnz, MPI_DOUBLE,
                      0, comm);
 
+        // Free allocation arrays
         free(sendcounts_nnz);
         free(displs_nnz);
     }
     else
     {
+        // Receive col_indices and values
         MPI_Scatterv(NULL, NULL, NULL, MPI_INT,
                      A_local->col_indices, A_local->nnz, MPI_INT,
                      0, comm);
@@ -209,6 +246,7 @@ int csr_build_spd_full(CSR *A, int grid_size)
 
     // Allocate row_ptr
     A->row_ptr = (int *)malloc((n + 1) * sizeof(int));
+
     if (!A->row_ptr)
     {
         fprintf(stderr, "Error allocating memory for row_ptr\n");
@@ -259,9 +297,11 @@ int csr_build_spd_full(CSR *A, int grid_size)
     // Allocate col_indices and values
     A->col_indices = (int *)malloc(nnz * sizeof(int));
     A->values = (double *)malloc(nnz * sizeof(double));
+
     if (!A->col_indices || !A->values)
     {
-        fprintf(stderr, "Failed to allocate CSR arrays\n");
+        // log error and free previously allocated memory
+        fprintf(stderr, "Error: Memory allocation failed for col_indices or values\n");
         free(A->row_ptr);
         return -1;
     }
@@ -314,17 +354,6 @@ int csr_build_spd_full(CSR *A, int grid_size)
         }
     }
 
-    // print the matrix memory usage
-    double a_value_size = nnz * sizeof(double);
-    double a_colind_size = nnz * sizeof(int);
-    double a_rowptr_size = (n + 1) * sizeof(int);
-    double total_size = a_value_size + a_colind_size + a_rowptr_size;
-    printf("CSR Matrix memory usage: \nvalues =  %.2f MB\n        col_indices = %.2f MB\n        row_ptr = %.2f MB\n        total = %.2f MB\n",
-           a_value_size / (1024.0 * 1024.0),
-           a_colind_size / (1024.0 * 1024.0),
-           a_rowptr_size / (1024.0 * 1024.0),
-           total_size / (1024.0 * 1024.0));
-
     return 0;
 }
 
@@ -355,4 +384,40 @@ void csr_free(CSR *A)
     A->n_local = 0;
     A->nnz = 0;
     A->row_start = 0;
+}
+
+void export_csr_to_mtx(const char *filename, CSR *A, int n, int rank)
+{
+    /***
+     * Exports a CSR matrix A to a Matrix Market (.mtx) file.
+     * so it would be easy to read in python and verify correctness of SPD matrix properties
+     ***/
+
+    FILE *f = fopen(filename, "w");
+    if (!f)
+    {
+        fprintf(stderr, "[rank %d]Error opening %s\n", rank, filename);
+        fflush(stderr);
+        return;
+    }
+
+    // Write Matrix Market header
+    fprintf(f, "%%MatrixMarket matrix coordinate real symmetric\n");
+    fprintf(f, "%d %d %d\n", n, n, A->nnz);
+
+    // Write non-zero entries
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = A->row_ptr[i]; j < A->row_ptr[i + 1]; j++)
+        {
+            int col = A->col_indices[j];
+            double val = A->values[j];
+            // Matrix Market format uses 1-based indexing
+            fprintf(f, "%d %d %.16e\n", i + 1, col + 1, val);
+        }
+    }
+    fclose(f);
+
+    fprintf(stdout, "[rank %d]Matrix exported to %s in Matrix Market format.\n", rank, filename);
+    fflush(stdout);
 }
--- src/csr.h
+++ src/csr.h
@@ -16,13 +16,18 @@ typedef struct
 int csr_build_spd_full(CSR *A, int grid_size);
 
 // Distributes the CSR matrix A from rank 0 to all processes, each receiving its local part in A_local
-int csr_distribute(CSR *A, CSR *A_local, int rank, int size, MPI_Comm comm);
+int csr_distribute(CSR *A, CSR *A_local, int n, int n_local, int row_start, int rank, int size, MPI_Comm comm);
 
 // SpMV: y_local = A_local * x
 void csr_sparse_matvec_mult_local(const CSR *A_local, const double *x_full, double *y_local);
 
 // Extracts the diagonal elements from the local CSR matrix A_local.
-void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local);
+void csr_get_diagonal_local(const CSR *A_local, double *diag_A_local, int rank);
+
+void print_csr_memory_usage(int rank, size_t nnz, int n_local, const char *label);
 
 // Free CSR matrix
 void csr_free(CSR *A);
+
+// Export CSR matrix to Matrix Market format for verification
+void export_csr_to_mtx(const char *filename, CSR *A, int n, int rank);
